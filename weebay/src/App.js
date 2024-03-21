e1f8f0dc16b2026bbdefa65a777396de826a7817
Csych97 <chrissych97@gmail.com>
2024-03-20 17:54:52 -0700

    hurrt

--- weebay/src/App.js
+++ weebay/src/App.js
@@ -1,11 +1,40 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import GetProducts from './components/GetProducts';
+import Registration from './components/Registration';
 
 function App() {
   return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/registration">Registration</Link>
+            </li>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+          </ul>
+        </nav>
+        <hr />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/registration' component={Registration} />
+          <Route path='/products' component={GetProducts} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+function Home() {
+  return (
     <div>
       <h1 style={{fontSize:"300%"}}>Weebay</h1>
-      <GetProducts />
+      <p>Welcome to Weebay. Our finest selection of fake clothing and accessories.</p>
     </div>
   );
 }
